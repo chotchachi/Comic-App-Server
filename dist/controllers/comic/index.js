97ca88f8ba066b0e25d47997a681948b083ef42a
ThanhQuang <quangtt@foxcode.org>
2020-12-20 23:10:48 +0700

    rename files

--- dist/controllers/comic/index.js
+++ dist/controllers/comic/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 
 const express = require("express");
-const detailController = require("./detail.controller");
+const detailController = require("./comic.controller");
 
 const router = express.Router();
 router.get('/', detailController.allComic);
